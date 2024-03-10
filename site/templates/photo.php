74ec917df6f24c4e9317c26872cd93bd19febe7a
Marc Hawkins <marchawkins@gmail.com>
2024-03-09 21:46:11 -0500

    photo styles

--- site/templates/photo.php
+++ site/templates/photo.php
@@ -1,13 +1,32 @@
 <?php snippet('header') ?>
 
+<?php
+    
+?>
 <main>
     <article>
         <h1><?php echo $page->title() ?></h1>
-        <?php if($page->files()->count() < 2): ?>
-            single
-        <?php else: ?>
-            multiple
+        <?php if($page->text()): ?>
+            <p><?php echo $page->text()->kirbytext() ?></p>
         <?php endif ?>
+
+        <div id="photos">
+        <?php foreach($page->files() as $file): ?>
+            <img loading="lazy" src="<?php echo $file->url() ?>" alt="<?php echo $page->title() ?> photo"/>
+        <?php endforeach ?>
+        </div><!-- #photos -->
+
+        <nav class="pagination">
+        <?php if ($page->hasPrevListed()): ?>
+            <a class="next" href="<?= $page->prevListed()->url() ?>" title="previous photo">older photos</a>
+        <?php endif ?>
+
+        <?php if ($page->hasNextListed()): ?>
+            <a class="prev" href="<?= $page->nextListed()->url() ?>" title="next photo">newer photos</a>
+        <?php endif ?>
+
+            <a href="/photos" title="all photos">all photos</a>
+        </nav>
     </article>
 </main>
 
--- site/templates/photos.php
+++ site/templates/photos.php
@@ -1,17 +1,33 @@
 <?php snippet('header') ?>
 
+<?php
+    $photos   = $page->children()->listed()->sortBy('date_taken', 'desc')->paginate(30);
+    $pagination = $photos->pagination();
+?>
 <main>
     <article>
         <h1><?php echo $page->title() ?></h1>
         <ul>
-            <?php foreach($page->children()->listed()->sortBy('date_taken', 'desc') as $subpage): ?>
+            <?php foreach($photos as $photopage): ?>
             <li>
-                <a href="<?= $subpage->url() ?>" title="<?php echo $subpage->title() ?>"><img loading="lazy" src="<?php echo $subpage->image()->crop(200,200)->url() ?>" alt="<?php echo $subpage->title() ?>"/></a>
+                <a href="<?= $photopage->url() ?>" title="<?php echo $photopage->title() ?>"><img loading="lazy" src="<?php echo $photopage->image()->crop(200,200)->url() ?>" alt="<?php echo $photopage->title() ?>"/></a>
             </li>
             <?php endforeach ?>
             <li></li>
         </ul>
     </article>
+
+    <?php if ($pagination->hasPages()): ?>
+        <nav class="pagination">
+        <?php if ($pagination->hasNextPage()): ?>
+            <a class="next" href="<?= $pagination->nextPageURL() ?>">older photos</a>
+        <?php endif ?>
+
+        <?php if ($pagination->hasPrevPage()): ?>
+            <a class="prev" href="<?= $pagination->prevPageURL() ?>">newer photos</a>
+        <?php endif ?>
+        </nav>
+    <?php endif ?>
 </main>
 
 <?php snippet('footer') ?>    
