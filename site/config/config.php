ce4989a0cbf7f39de2fc26ec35d10968c031a4e5
Marc <marchawkins@gmail.com>
2025-02-17 13:57:26 -0500

    remove guestbook debug

--- site/config/config.php
+++ site/config/config.php
@@ -5,7 +5,7 @@ return [
     //     'install' => true
     //  ]
     //'debug' => true
-    'mauricerenck.komments.debug' => true,
+    'mauricerenck.komments.debug' => false,
     'notifications.email.enable' => true,
     'notifications.email.sender' => 'no-reply@marchawkins.com',
     'notifications.email.emailReceiverList' => 'marchawkins@gmail.com'
