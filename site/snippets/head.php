27af1bb8faad5aa9a27bc4fa4760ead4db7171e4
Marc <marchawkins@gmail.com>
2025-03-08 13:14:23 -0500

    seo keyword and description functionality

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -4,6 +4,34 @@
     <?php snippet('ascii-title01') ?> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page->title() ?> | <?= $site->title() ?></title>
+    <?php
+        $metaTags = ['marc hawkins','marchawkins.com'];
+        array_push($metaTags,$page->title());
+        if($page->year_start()->isNotEmpty()):
+            array_push($metaTags,$page->year_start());
+        endif;
+        if($page->tags()->isNotEmpty()):
+            $tagsArray = $page->tags()->split();
+            $metaTags = array_merge($metaTags, $tagsArray);
+        endif;
+        $keywords = implode(', ', $metaTags);
+    ?>
+    <meta name="keywords" content="<?php echo htmlspecialchars($keywords, ENT_QUOTES, 'UTF-8'); ?>"/>
+    
+    <?php
+        $metaDescription = "";
+        $maxLength = 155;
+        if($page->text()->isNotEmpty()):
+            $metaDescription .= $page->text();
+            if (strlen($metaDescription) > $maxLength):
+                $metaDescription = substr($metaDescription, 0, $maxLength)."...";
+            endif;
+        else:
+            $metaDescription .= $page->title()." from marchawkins.com";
+        endif;
+    ?>
+    <meta name="description" content="<?php echo htmlspecialchars($metaDescription, ENT_QUOTES, 'UTF-8'); ?>"/>
+
     <?php if($page->title()!='Home'): ?>
         <?= css('assets/css/styles.css'); ?>
     <?php endif ?>
@@ -17,6 +45,5 @@
     <?= css('@auto'); ?>
     <?= css($page->files()->filterBy('extension', 'css')->pluck('url')) ?>
     <?= js($page->files()->filterBy('extension', 'js')->pluck('url')) ?>
-
 </head>
 <body id="<?= $page->slug() ?>">
