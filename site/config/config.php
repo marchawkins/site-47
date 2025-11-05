bd1a73cacee91d1b53dbb0066032d1e57944856b
Marc <marchawkins@gmail.com>
2025-11-05 10:24:15 -0500

    adds sitemap

--- site/config/config.php
+++ site/config/config.php
@@ -8,6 +8,27 @@ return [
 
     'routes' => [
         [
+            'pattern' => 'sitemap.xml',
+            'action'  => function() {
+                $pages = site()->pages()->index();
+
+                // fetch the pages to ignore from the config settings,
+                // if nothing is set, we ignore the error page
+                $ignore = kirby()->option('sitemap.ignore', ['error']);
+
+                $content = snippet('sitemap', compact('pages', 'ignore'), true);
+
+                // return response with correct header type
+                return new Kirby\Cms\Response($content, 'application/xml');
+            }
+        ],
+        [
+            'pattern' => 'sitemap',
+            'action'  => function() {
+                return go('sitemap.xml', 301);
+            }
+        ],
+        [
             'pattern' => '(:any)',
             'action'  => function($uid) {
                 $page = page($uid);
@@ -23,7 +44,7 @@ return [
             }
         ]
     ],
-
+    'sitemap.ignore' => ['error'],
     'mauricerenck.komments.debug' => false,
     'mauricerenck.komments.storage.type' => 'markdown',
     'notifications.email.enable' => true,
