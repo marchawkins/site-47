a21836c38f4698e5f8560f128121861557af1316
Marc <marchawkins@gmail.com>
2025-11-04 21:49:04 -0500

    setup slash page structure (not public)

--- site/config/config.php
+++ site/config/config.php
@@ -4,7 +4,26 @@ return [
     // 'panel' =>[
     //     'install' => true
     //  ]
-    'debug' => true,
+    //'debug' => true,
+
+    'routes' => [
+        [
+            'pattern' => '(:any)',
+            'action'  => function($uid) {
+                $page = page($uid);
+                if (!$page) $page = page('slashes/' . $uid);
+                if (!$page) $page = site()->errorPage();
+                return site()->visit($page);
+            }
+        ],
+        [
+            'pattern' => 'slashes/(:any)',
+            'action'  => function($uid) {
+                go($uid);
+            }
+        ]
+    ],
+
     'mauricerenck.komments.debug' => false,
     'mauricerenck.komments.storage.type' => 'markdown',
     'notifications.email.enable' => true,
