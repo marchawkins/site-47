26105108e16ab73bf51176b902939bcf4f1277d4
Marc <marchawkins@gmail.com>
2025-11-10 11:12:40 -0500

    initial /ai page

--- site/templates/slashes.ai.php
+++ site/templates/slashes.ai.php
@@ -6,8 +6,10 @@
     ?>
 
     <article>
-        <p>AI template</p>
-        <h1><?= $page->title() ?></h1>
+        <div class="slashes-header">
+            <img src="/assets/images/slashes-ai-cow-header.jpg" alt="AI header photo of a robot holding steak sauce in front of a field of cows.">
+        </div>
+        <h1>/<?= $page->title() ?></h1>
         <p><?php echo $page->text()->kirbytext() ?></p>
 
     </article>
