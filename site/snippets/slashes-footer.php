3561568fa30f8f12f622a39f57d4c85bc42127cc
Marc <marchawkins@gmail.com>
2025-11-10 13:54:32 -0500

    finished AI page; add back nav to slash pages

---
+++ site/snippets/slashes-footer.php
@@ -0,0 +1,3 @@
+<nav class="pagination">
+    <a href="/slashes" title="all slash pages">all slash pages</a>
+</nav>
--- site/templates/slashes.ai.php
+++ site/templates/slashes.ai.php
@@ -6,12 +6,11 @@
     ?>
 
     <article>
-        <div class="slashes-header">
-            <img src="/assets/images/slashes-ai-cow-header.jpg" alt="AI header photo of a robot holding steak sauce in front of a field of cows.">
-        </div>
         <h1>/<?= $page->title() ?></h1>
         <p><?php echo $page->text()->kirbytext() ?></p>
 
+        <?php snippet('slashes-footer') ?>
+         
     </article>
     
 </main>
