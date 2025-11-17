b68d4e554eef1939f1c4682ab20837e51fd242ca
Marc <marchawkins@gmail.com>
2025-11-17 17:21:46 -0500

    more WIP canon

--- site/templates/slashes.canon.php
+++ site/templates/slashes.canon.php
@@ -6,8 +6,15 @@
         <h1>/<?= $page->title() ?></h1>
         <p><?php echo $page->text()->kirbytext() ?></p>
 
+        <?php foreach ($page->items()->toStructure() as $item): ?>
+            <div class="canon-item">
+                <h3><?= $item->title() ?></h3>
+                <p><?= $item->description() ?></p>
+                <p><?= $item->url() ?></p>
+            </div><!-- .canon-item -->
+        <?php endforeach ?>
+
         <?php snippet('slashes-footer') ?>
-         
     </article>
     
 </main>
