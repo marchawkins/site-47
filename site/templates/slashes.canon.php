2c261529baec8d4dc2a1cf1b04e1923f51258a3f
Marc <marchawkins@gmail.com>
2025-11-18 19:58:51 -0500

    More WIP Canon

--- site/templates/slashes.canon.php
+++ site/templates/slashes.canon.php
@@ -9,8 +9,12 @@
         <?php foreach ($page->items()->toStructure() as $item): ?>
             <div class="canon-item">
                 <h3><?= $item->title() ?></h3>
-                <p><?= $item->description() ?></p>
+                <p style="display: none;"><?= $item->description() ?></p>
                 <p><?= $item->url() ?></p>
+                <p><?= $item->year() ?></p>
+                <?php if($thumbURL = $item->thumbnail()->toFile()->url()): ?>
+                    <img src="<?= $thumbURL?>" alt="thumbnail for <?= $item->title() ?>">
+                <?php endif ?> 
             </div><!-- .canon-item -->
         <?php endforeach ?>
 
