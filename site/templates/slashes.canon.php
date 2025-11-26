7a33b9c430615428ffa4fad9335d1467845cad79
Marc <marchawkins@gmail.com>
2025-11-26 13:26:26 -0500

    Basic styling for canon
    Adds simple horizontal formatting on desktop, stacked on mobile.

--- site/templates/slashes.canon.php
+++ site/templates/slashes.canon.php
@@ -7,15 +7,25 @@
         <p><?php echo $page->text()->kirbytext() ?></p>
 
         <?php foreach ($page->items()->toStructure() as $item): ?>
-            <div class="canon-item">
-                <h3><?= $item->title() ?></h3>
-                <p style="display: none;"><?= $item->description() ?></p>
-                <p><?= $item->url() ?></p>
-                <p><?= $item->year() ?></p>
-                <?php if($thumbURL = $item->thumbnail()->toFile()->url()): ?>
-                    <img src="<?= $thumbURL?>" alt="thumbnail for <?= $item->title() ?>">
-                <?php endif ?> 
-            </div><!-- .canon-item -->
+            <dl>
+                <dt>
+                    <?php if($thumbURL = $item->thumbnail()->toFile()->url()): ?>
+                        <?php if($item->url()): ?><a href="<?= $item->url() ?>"><?php endif; ?>
+                            <img src="<?= $thumbURL?>" alt="thumbnail for <?= $item->title() ?>">
+                        <?php if($item->url()): ?></a><?php endif; ?>
+                    <?php else: ?>
+                        <?php $thumbURL = false; ?>
+                        <h3><?= $item->title() ?></h3>
+                    <?php endif ?> 
+                </dt>
+                <dd>
+                    <?php if($thumbURL): ?>
+                        <h3><?php if($item->url()): ?><a href="<?= $item->url() ?>"><?php endif; ?><?= $item->title() ?><?php if($item->url()): ?></a><?php endif; ?></h3>
+                    <?php endif ?>
+                    <p><?= $item->description() ?></p>
+                    <p><?= $item->year() ?></p>
+                </dd>
+            </dl><!-- .canon-item -->
         <?php endforeach ?>
 
         <?php snippet('slashes-footer') ?>
